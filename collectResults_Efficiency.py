7508ae7c91ea24f1d55138edb75ad7f6356fabf5
Michael Hahn <mhahn29@gmail.com>
2020-06-20 21:35:56 -0700

    code

---
+++ collectResults_Efficiency.py
@@ -0,0 +1,46 @@
+import os
+import sys
+
+group = sys.argv[1]
+
+PATH = "/u/scr/mhahn/deps/"+group+"/"
+
+files = os.listdir(PATH)
+
+cache = {}
+
+outHeader = set(["Language"])
+
+entries = []
+
+for filename in files:
+   if "model" in filename:
+      print("READING "+filename )
+      part1 = filename.split("_model_")[0]
+      if "_" in part1:
+        language = part1.split("_")[0]
+      else:
+        assert False
+      with open(PATH+filename, "r") as inFile:
+          try:
+            header = next(inFile).strip().split("\t")
+          except StopIteration:
+            print ["EMPTY FILE?",inPath+filename]
+            continue
+          for x in header:
+            outHeader.add(x)
+          for line in inFile:
+             line = line.strip().split("\t")
+             if len(line) < 2:
+                continue
+             entry = dict(list(zip(header, line)))
+             entry["Language"] = language
+             entries.append(entry)
+outHeader = sorted(list(outHeader))
+print(outHeader)
+
+with open(PATH+"auto-summary-lstm.tsv", "w") as outFile:
+  print("\t".join(outHeader) , file=outFile)
+  for entry in entries:
+    print("\t".join([entry.get(x, "NA") for x in outHeader]), file=outFile)
+print("WRITING", PATH+"auto-summary-lstm.tsv")
