fbecb34c8d78bd03c55efa5fcd609fa9e55e037e
Michael Hahn <mhahn29@gmail.com>
2020-07-19 21:37:07 -0700

    code

---
+++ optimizeDLM/perSentence/optimizeDependencyLength_POS_NoSplit_ByOcc_Coarse_FuncHead_IdentifyOptimalOrders_MLE_Collect_All.py
@@ -0,0 +1,12 @@
+from ud_languages import languages
+
+
+import random
+random.shuffle(languages)
+import subprocess
+
+with open("outputs/"+__file__+".tsv", "w") as outFile:
+   print("\t".join(["Language", "Fraction", "DepLen", "DepLenBest"]), file=outFile)
+for language in languages:
+   subprocess.call(["/u/nlp/anaconda/ubuntu_16/envs/py27-mhahn/bin/python2.7", __file__.replace("_All.py", ".py"), language])
+
