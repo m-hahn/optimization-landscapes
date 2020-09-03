b489dd3292635aa1fdefe4a99d0114723c3de618
Michael Hahn <mhahn29@gmail.com>
2020-09-03 21:43:58 +0800

    code

---
+++ optimizeDLM/perSentence/collectSentencesProperties_IncludingNonNouns_All.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+keys = {}
+import sys
+
+from ud_languages import languages
+
+import random
+import subprocess
+
+script = 'collectSentencesProperties_IncludingNonNouns.py'
+
+
+with open("outputs/"+script+".tsv", "w") as outFile:
+   print >> outFile, ("Language\tcorrelatingDependents\tisRoot\tobjects\torder\tsubjectLength\tverbDependents\tverbLength")
+for language in languages:
+   subprocess.call(['/u/nlp/anaconda/ubuntu_16/envs/py27-mhahn/bin/python2.7', script, "--language="+language])
+
+#   break
+
