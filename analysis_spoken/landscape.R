2ecc5143c28540fb216a9b3e560c566b58d79a79
Michael Hahn <mhahn29@gmail.com>
2020-09-08 21:34:29 +0800

    code

--- analysis_spoken/landscape.R
+++ analysis_spoken/landscape.R
@@ -5,7 +5,7 @@ spoken = read.csv("spoken.tsv", sep="\t")
 
 library(ggplot2)
 
-plot = ggplot(spoken, aes(x=OSSameSide_Real_Prob, y=OSSameSide)) + geom_label(aes(label=Language)) + xlim(0,1) + ylim(0,1) + xlab("Real") + ylab("Predicted")
+plot = ggplot(spoken, aes(x=OSSameSide_Real_Prob, y=OSSameSide)) +geom_label(aes(label=Language, y=OSSameSide_Other), color="gray", linetype=2) + geom_label(aes(label=Language, y=OSSameSide)) + xlim(0,1) + ylim(0,1) + xlab("Real") + ylab("Predicted")
 ggsave(plot, file="spoken.pdf")
 
 
