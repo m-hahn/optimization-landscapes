17fb80bf4cfda28c117fb80d68b298d2a330b3b9
Michael Hahn <mhahn29@gmail.com>
2020-09-24 13:12:03 +0800

    code

---
+++ change/ornuhl-binom-geo/20model.py
@@ -0,0 +1,168 @@
+#import pystan
+from math import sqrt, log
+import math
+
+with open("../geolocations.tsv", "r") as inFile:
+  geolocations = [x.split("\t") for x in inFile.read().strip().split("\n")][1:]
+print(geolocations)
+languages = [x[1] for x in geolocations]
+latitudes = [float(x[4]) for x in geolocations]
+longitudes = [float(x[5]) for x in geolocations]
+latitudes = dict(list(zip(languages, latitudes)))
+longitudes = dict(list(zip(languages, longitudes)))
+
+
+with open("../trees.tsv", "r") as inFile:
+  trees = [x.split("\t") for x in inFile.read().strip().split("\n")][1:]
+print(trees)
+
+observedLangs = set()
+allLangs = set()
+parents = {}
+for line in trees:
+   observedLangs.add(line[0])
+   allLangs.add(line[0])
+   for i in range(len(line)-1):
+       child = line[i]
+       parent = line[i+1]
+       if child in parents:
+         assert parents[child] == parent, (parents[child], line, child, i)
+       parents[child] = parent
+       allLangs.add(parent)
+
+with open("../groups.tsv", "r") as inFile:
+  dates = [x.split("\t") for x in inFile.read().strip().split("\n")][1:]
+print([len(x) for x in dates])
+dates = dict(dates)
+dates["_ROOT_"] = -50000
+print(dates)
+for x in allLangs:
+  if x not in observedLangs:
+    if x not in dates:
+       print(x)
+
+with open("../../landscapes_2.6.R.tsv", "r") as inFile:
+   data = [x.replace('"', '').split(" ") for x in inFile.read().strip().split("\n")]
+header = data[0]
+header = ["ROWNUM"] + header
+header = dict(list(zip(header, range(len(header)))))
+data = data[1:]
+print(header)
+valueByLanguage = {}
+for line in data:
+   language = line[header["Language"]]
+   if language == "Afrikaans_2.6":
+     continue
+   if language == "Ancient_Greek_2.6":
+     continue
+   x = int(line[header["OSSameSideSum"]])
+   y = int(line[header["OSSameSideTotal"]])
+   z = float(line[header["OSSameSide_Real_Prob"]])
+   valueByLanguage[language] = [x,y,z] 
+
+valueByLanguage["ISWOC_Old_English"] = [10, 13, 0.49]
+valueByLanguage["Archaic_Greek"] = [8, 10, 0.56]
+valueByLanguage["Classical_Greek"] = [5, 10, 0.52]
+valueByLanguage["Koine_Greek"] = [6, 10, 0.47]
+
+print(valueByLanguage)
+
+print(observedLangs)
+print(allLangs)
+hiddenLangs = [x for x in allLangs if x not in observedLangs]
+print(hiddenLangs)
+
+#observedLanguages = [x for x in list(observedLangs) if parents[x] not in observedLangs] # This is for understanding what the model does on only synchronic data
+observedLanguages = [x for x in list(observedLangs) if x in valueByLanguage]
+hiddenLanguages = hiddenLangs
+totalLanguages = ["_ROOT_"] + hiddenLanguages + observedLanguages
+lang2Code = dict(list(zip(totalLanguages, range(len(totalLanguages)))))
+lang2Observed = dict(list(zip(observedLanguages, range(len(observedLanguages)))))
+
+distanceToParent = {}
+for language in allLangs:
+   parent = parents.get(language, "_ROOT_")
+   if language in observedLangs:
+     assert parent != "_ROOT_", language
+   dateLang = dates.get(language, 2000)
+   dateParent = dates[parent]
+   distanceToParent[language] = (float(dateLang)-float(dateParent))/1000
+print(distanceToParent)
+
+from collections import defaultdict
+fromParentsToDescendants = defaultdict(list)
+for lang, parent in parents.items():
+   fromParentsToDescendants[parent].append(lang)
+fromParentsToDescendants["_ROOT_"] = []
+for lang in fromParentsToDescendants:
+    if lang not in parents and lang != "_ROOT_":
+        fromParentsToDescendants["_ROOT_"].append(lang)
+print(fromParentsToDescendants)
+
+def mean(x):
+    return sum(x)/len(x)
+
+done = set()
+def getGeolocation(lang):
+   if lang in done:
+       return
+   for lang2 in fromParentsToDescendants[lang]:
+       assert lang2 != lang, lang
+       getGeolocation(lang2)
+   if lang not in latitudes:
+     latitudes[lang] = mean([latitudes[x] for x in fromParentsToDescendants[lang]])
+     longitudes[lang] = mean([longitudes[x] for x in fromParentsToDescendants[lang]])
+   done.add(lang)
+
+getGeolocation("_ROOT_")
+print(latitudes)
+print(longitudes)
+
+totalLanguages = ["_ROOT_"] + hiddenLanguages + observedLanguages
+
+import geopy.distance
+
+kernel = [[0 for _ in range(len(totalLanguages))] for _ in range(len(totalLanguages))]
+for i in range(len(kernel)):
+   for j in range(i):
+     l1 = totalLanguages[i]
+     l2 = totalLanguages[j]
+     lat1, long1 = latitudes[l1], longitudes[l1]
+     lat2, long2 = latitudes[l2], longitudes[l2]
+     distance = geopy.distance.geodesic((lat1, long1), (lat2, long2)).km/10000
+#     print(lat1, long1, lat2, long2, l1, l2, geopy.distance.geodesic((lat1, long1), (lat2, long2)).km/10000)
+     kernel[i][j] = distance
+     kernel[j][i] = distance
+
+dat = {}
+
+dat["ObservedN"] = len(observedLanguages)
+dat["TrialsSuccess"] = [valueByLanguage[x][0] for x in observedLanguages]
+dat["TrialsTotal"] = [valueByLanguage[x][1] for x in observedLanguages]
+dat["TraitObserved"] = [valueByLanguage[x][2] for x in observedLanguages]
+dat["HiddenN"] = len(hiddenLanguages)+1
+dat["TotalN"] = dat["ObservedN"] + dat["HiddenN"]
+assert dat["TotalN"] == len(totalLanguages)
+dat["IsHidden"] = [1]*dat["HiddenN"] + [0]*dat["ObservedN"]
+dat["ParentIndex"] = [0] + [1+lang2Code[parents.get(x, "_ROOT_")] for x in hiddenLanguages+observedLanguages]
+dat["Total2Observed"] = [0]*dat["HiddenN"] + list(range(1,1+len(observedLanguages)))
+dat["Total2Hidden"] = [1] + list(range(2,2+len(hiddenLanguages))) + [0 for _ in observedLanguages]
+dat["ParentDistance"] = [0] + [distanceToParent.get(x, 10) for x in hiddenLanguages+observedLanguages]
+dat["prior_only"] = 0
+dat["Components"] = 2
+dat["DistanceMatrix"] = kernel
+print(dat)
+
+sm = pystan.StanModel(file='20model.stan')
+
+
+fit = sm.sampling(data=dat, iter=2000, chains=4)
+la = fit.extract(permuted=True)  # return a dictionary of arrays
+print(fit)
+print(la)
+print("Inferred logits", la["LogitsAll"].mean(axis=0))
+print("Inferred hidden traits", la["TraitHidden"].mean(axis=0))
+print("alpha", la["alpha"].mean(axis=0))
+print("sigma_B", la["sigma_B"].mean(axis=0))
+print("Lrescor_B", la["Lrescor_B"].mean(axis=0))
+
