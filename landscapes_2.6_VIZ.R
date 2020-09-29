68f58eb96dd952fabe97857bfeed2480dc455a43
Michael Hahn <mhahn29@gmail.com>
2020-09-29 21:04:19 +0800

    code

---
+++ landscapes_2.6_VIZ.R
@@ -0,0 +1,74 @@
+library(tidyr)
+library(dplyr)
+
+SCR = "~/CS_SCR/"
+DEPS = paste(SCR,"/deps/", sep="")
+#DEPS = "/u/scr/mhahn/deps/"
+data = read.csv(paste(DEPS, "DLM_MEMORY_OPTIMIZED/locality_optimized_dlm/manual_output_funchead_fine_depl", "/", "auto-summary-lstm_2.6.tsv", sep=""), sep="\t")
+dataBackup = data
+data = data %>% filter(HeadPOS == "VERB", DependentPOS == "NOUN") %>% select(-HeadPOS, -DependentPOS)
+
+
+
+#DLM_MEMORY_OPTIMIZED/locality_optimized_dlm/manual_output_funchead_fine_depl
+
+
+dataO = data %>% filter(CoarseDependency == "obj")
+dataS = data %>% filter(CoarseDependency == "nsubj")
+
+data = merge(dataO, dataS, by=c("Language", "FileName"))
+
+data = data %>% mutate(OFartherThanS = (DistanceWeight.x > DistanceWeight.y))
+data = data %>% mutate(OSSameSide = (sign(DH_Weight.x) == sign(DH_Weight.y)))
+
+data = data %>% mutate(Order = ifelse(OSSameSide & OFartherThanS, "VSO", ifelse(OSSameSide, "SOV", "SVO")))
+
+families = read.csv("families.tsv", sep="\t")
+data = merge(data, families, by=c("Language"), all.x=TRUE)
+
+
+u = data %>% group_by(Language, Family) %>% summarise(OSSameSideSum=sum(OSSameSide), OSSameSideTotal=NROW(OSSameSide), OSSameSide = mean(OSSameSide), OFartherThanS = mean(OFartherThanS))
+print(u[order(u$OSSameSide),], n=60)
+
+sigmoid = function(x) {
+	return(1/(1+exp(-x)))
+}
+
+real = read.csv(paste(SCR,"/deps/LANDSCAPE/mle-fine_selected/auto-summary-lstm_2.6.tsv", sep=""), sep="\t")
+realO = real %>% filter(Dependency == "obj")
+realS = real %>% filter(Dependency == "nsubj")
+
+real = merge(realO, realS, by=c("Language", "FileName", "ModelName"))
+
+real = real %>% mutate(OFartherThanS_Real = (Distance_Mean_NoPunct.x > Distance_Mean_NoPunct.y))
+real = real %>% mutate(OSSameSide_Real = (sign(DH_Mean_NoPunct.x) == sign(DH_Mean_NoPunct.y)))
+real = real %>% mutate(OSSameSide_Real_Prob = (sigmoid(DH_Mean_NoPunct.x) * sigmoid(DH_Mean_NoPunct.y)) + ((1-sigmoid(DH_Mean_NoPunct.x)) * (1-sigmoid(DH_Mean_NoPunct.y))))
+
+real = real %>% mutate(Order_Real = ifelse(OSSameSide_Real & OFartherThanS_Real, "VSO", ifelse(OSSameSide_Real, "SOV", "SVO")))
+
+u = merge(u, real %>% select(Language, OSSameSide_Real, OSSameSide_Real_Prob), by=c("Language"))
+
+
+write.table(u, file="landscapes_2.6_new.R.tsv")
+
+u = u %>% mutate(LogOSSameSide = log(OSSameSide+1e-10))
+u = u %>% mutate(LogOSSameSide_Real_Prob = log(OSSameSide_Real_Prob+1e-10))
+
+data = merge(data, real, by=c("Language"))
+
+
+data$OSSameSide_Real_Prob_Log = log(data$OSSameSide_Real_Prob)
+
+library(stringr)
+u$Language2 = as.character(u$Language)
+for(i in (1:nrow(u))) {
+	u$Language2[[i]] = str_replace(u$Language2[[i]], "_2.6", "")
+	u$Language2[[i]] = str_replace(u$Language2[[i]], "_", " ")
+}
+
+
+library(ggrepel)
+library(ggplot2)
+plot = ggplot(u, aes(x=OSSameSide_Real_Prob, y=OSSameSide, color=Family)) + geom_text_repel(aes(label=Language2)) + xlab("Real Subject-Object Symmetry") + ylab("Optimal Subject-Object Symmetry") + theme_bw() + theme(legend.position="none", axis.text=element_text(size=14), axis.title=element_text(size=16)) + theme(panel.grid = element_blank())
+ggsave("figures/fracion-optimized_DLM_2.6_format.pdf", height=7, width=7)
+
