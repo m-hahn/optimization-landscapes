12465ea4a3dc83bdd88a7c49a71424a3d0520b90
m-hahn <mhahn29@gmail.com>
2020-10-05 20:24:30 +0800

    Updates from Overleaf

---
+++ change/README.md
@@ -0,0 +1,7 @@
+`brownian-binom`
+`ornuhl-binom`
+`ornuhl-binom-geo`
+`ornuhl-binom-geo-time`
+`ornuhl-joint`
+`trees`
+`visualize`
--- change/ornuhl-binom/noLatents/46model.py
+++ change/ornuhl-binom/noLatents/46model.py
@@ -69,6 +69,7 @@ totalLanguages = ["_ROOT_"] + hiddenLanguages + observedLanguages
 lang2Code = dict(list(zip(totalLanguages, range(len(totalLanguages)))))
 lang2Observed = dict(list(zip(observedLanguages, range(len(observedLanguages)))))
 
+
 distanceToParent = {}
 for language in allLangs:
    parent = parents.get(language, "_ROOT_")
@@ -81,6 +82,63 @@ print(distanceToParent)
 print(parents.get("Classical_Chinese_2.6"))
 assert "Classical_Chinese_2.6" in observedLangs
 #quit()
+
+from collections import defaultdict
+
+
+listOfAllAncestors = defaultdict(list)
+
+def processDescendants(l, desc):
+   listOfAllAncestors[desc].append(l)
+   if l != "_ROOT_":
+      parent = parents.get(l, "_ROOT_")
+      processDescendants(parent, desc)
+   
+for language in totalLanguages:
+   processDescendants(language, language)
+print(listOfAllAncestors)
+
+
+
+
+covarianceMatrix = [[None for _ in range(len(observedLanguages))] for _ in range(len(observedLanguages))]
+for i in range(len(observedLanguages)):
+   for j in range(i+1):
+      l1 = observedLanguages[i]
+      l2 = observedLanguages[j]
+#      print("----------")
+ #     print([l for l in listOfAllAncestors[l1] if l in listOfAllAncestors[l2]])
+  #    print([int(dates.get(l, 2000)) for l in listOfAllAncestors[l1] if l in listOfAllAncestors[l2]])
+      commonAncestorsDates = [int(dates.get(l, 2000)) for l in listOfAllAncestors[l1] if l in listOfAllAncestors[l2] and l != "_ROOT_"]
+      if len(commonAncestorsDates) > 0:
+         commonTime = max(commonAncestorsDates)
+      else:
+         commonTime = -50000
+      separateTime1 = (int(dates.get(l1, 2000)) - commonTime)
+      separateTime2 = (int(dates.get(l2, 2000)) - commonTime)
+      if commonTime > -50000:
+         print(l1, l2, separateTime1, separateTime2)
+      else:
+         separateTime1 = 1000000
+         separateTime2 = 1000000
+      covarianceMatrix[i][j] = (separateTime1)/1000
+      covarianceMatrix[j][i] = (separateTime2)/1000
+
+families = defaultdict(list)
+for language in observedLanguages:
+  ancestors = listOfAllAncestors[language]
+  print(ancestors)
+  assert ancestors[-1] == "_ROOT_"
+  families[([language] + ancestors)[-2]].append(language)
+print(families)
+familiesLists = [[observedLanguages.index(x)+1 for x in z] for _, z in families.items()]
+print(familiesLists)
+familiesListsMaxLen = max([len(x) for x in familiesLists])
+familiesLists = [x+[0 for _ in range(familiesListsMaxLen-len(x))] for x in familiesLists]
+print(familiesLists)
+
+
+
 
 dat = {}
 
@@ -95,8 +153,13 @@ dat["ParentIndex"] = [0] + [1+lang2Code[parents.get(x, "_ROOT_")] for x in hidde
 dat["Total2Observed"] = [0]*dat["HiddenN"] + list(range(1,1+len(observedLanguages)))
 dat["Total2Hidden"] = [1] + list(range(2,2+len(hiddenLanguages))) + [0 for _ in observedLanguages]
 dat["ParentDistance"] = [0] + [distanceToParent[x] for x in hiddenLanguages+observedLanguages]
+dat["CovarianceMatrix"] = covarianceMatrix
 dat["prior_only"] = 0
 dat["Components"] = 2
+dat["FamiliesLists"] = familiesLists
+dat["FamiliesNum"] = len(familiesLists)
+dat["FamiliesSize"] = len(familiesLists[0])
+
 
 print(dat)
 
@@ -105,13 +168,15 @@ sm = pystan.StanModel(file=f'{__file__[:-3]}.stan')
 
 fit = sm.sampling(data=dat, iter=2000, chains=4)
 la = fit.extract(permuted=True)  # return a dictionary of arrays
-
+import numpy as np
 with open(f"fits/{__file__}.txt", "w") as outFile:
    print(fit, file=outFile)
-#   print(la, file=outFile)
-#print("Inferred logits", la["LogitsAll"].mean(axis=0))
-#print("Inferred hidden traits", la["TraitHidden"].mean(axis=0))
-#print("alpha", la["alpha"].mean(axis=0))
-#print("sigma_B", la["sigma_B"].mean(axis=0))
-#print("Lrescor_B", la["Lrescor_B"].mean(axis=0))
-#
+print((la["Lrescor_Sigma"] > 0).mean(axis=0))
+print((la["Sigma"] > 0).mean(axis=0))
+print((la["Omega"] > 0).mean(axis=0))
+# Correlation
+#print(la["Sigma"][1,2] / (la["Sigma"][1,1].sqrt() * la["Sigma"][2,2].sqrt()))
+with open(f"fits/CORR_Sigma_{__file__}.txt", "w") as outFile:
+  for x in la["Sigma"][:,0,1] / np.sqrt(la["Sigma"][:,0,0] * la["Sigma"][:,1,1]):
+      print(float(x), file=outFile)
+
