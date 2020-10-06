1adb7df2f7bd0dcdb7ae58cf2494986b0e84e13f
Michael Hahn <mhahn29@gmail.com>
2020-10-06 00:13:39 -0700

    code

---
+++ analysis/README.md
@@ -0,0 +1,15 @@
+# Analysis
+
+* `analysis_spoken`
+* `case_marking`
+* `coexpression`
+* `families.tsv`
+* `figures`
+* `geolocations`
+* `landscapes_2.6_new.R.tsv`
+* `landscapes_2.6.R`
+* `landscapes_2.6_VIZ.R`
+* `output`
+* `pro_drop`
+* `visualize_historical`
+* `WALS`
--- change/solve.py
+++ change/solve.py
@@ -1,6 +1,6 @@
 import numpy as np
 B= np.matrix([[0.3, 0.7], [0.1, -0.2]])
-factor = np.matrix([[2*B[0,0], B[0,1], 0], [B[1,0], B[0,0]+B[1,1], B[0,1]], [0, B[1,0], 2*B[1,1]]])
+factor = np.matrix([[2*B[0,0], 2*B[0,1], 0], [B[1,0], B[0,0]+B[1,1], B[0,1]], [0, 2*B[1,0], 2*B[1,1]]])
 Sigma = np.matrix([[1, 0.2], [0.2, 1.2]])
 instant_cov_components = np.matrix([[Sigma[0,0]], [Sigma[0,1]], [Sigma[1,1]]])
 Omega_components = np.matmul(np.linalg.inv(factor), instant_cov_components)
