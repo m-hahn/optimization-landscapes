1e2ab4760a907ea3c9cafe7dcaebad1db72ca8d4
Michael Hahn <mhahn29@gmail.com>
2020-10-06 21:36:31 -0700

    code

---
+++ collectPerVerbProperties/coexpression_all.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+keys = {}
+import sys
+
+from ud_languages import languages
+
+import random
+import subprocess
+
+group = sys.argv[1]
+
+script = 'coexpression'+str(group)+'.py'
+
+
+with open("outputs/"+script+".tsv", "w") as outFile:
+   print >> outFile, "\t".join(['Language', 'No', 'Yes'])
+for language in languages:
+   subprocess.call(['/u/nlp/anaconda/ubuntu_16/envs/py27-mhahn/bin/python2.7', script, "--language="+language])
+
+#   break
+
