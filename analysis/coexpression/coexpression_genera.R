168d72ecaab0c2f35413154d1cb14e2919765d04
Michael Hahn <mhahn29@gmail.com>
2021-04-29 16:52:05 +0800

    code

--- analysis/coexpression/coexpression_genera.R
+++ analysis/coexpression/coexpression_genera.R
@@ -44,8 +44,8 @@ u$Coexpression = (u$Yes/(u$Yes+u$No))
 
 library(lme4)
 library(brms)
-model = ((brm(Coexpression ~ OSSameSide_Real_Prob + (1+OSSameSide_Real_Prob|Genus), data=u))) # (1+OSSameSide_Real_Prob|Family) + 
-
+u$OSSameSide_Real_Prob.C = u$OSSameSide_Real_Prob - mean(u$OSSameSide_Real_Prob, na.rm=TRUE)
+model = ((brm(Coexpression ~ OSSameSide_Real_Prob.C + (1+OSSameSide_Real_Prob.C|Genus), data=u))) # (1+OSSameSide_Real_Prob|Family) + 
 
 
 
@@ -62,6 +62,29 @@ cat("95% CrI [",quantile(samples$b_OSSameSide_Real_Prob, 0.025), " ", quantile(s
 cat("P(beta) > 0 ", mean(samples$b_OSSameSide_Real_Prob > 0), "\n")
 sink()
 
+
+
+
+genera = c("Afro_Asiatic", "Indo_European", "Niger-Congo", "Sino-Tibetan", "Turkic", "Uralic")
+slopes = data.frame(Genus=c(), Coefficient=c())
+for(genus in unique(u$Genus)) {
+        coeffs = samp$b_OSSameSide_Real_Prob.C + samp[[paste("r_Genus[", genus, ",OSSameSide_Real_Prob.C]", sep="")]]
+        slopes2 = data.frame(Coefficient=coeffs)
+        slopes2$Genus=genus
+        slopes=rbind(slopes, slopes2)
+}
+
+
+
+library(ggplot2)
+plot = ggplot(slopes %>% filter(Genus %in% genera), aes(x=Coefficient, group=Genus, color=Genus)) + geom_density(aes(y=..scaled..)) + theme_bw() + xlab("Coefficient in Mixed-Effects Regression") + ylab("Posterior Density") + geom_segment(aes(x=c(0), y=c(0), xend=c(0), yend=c(1), group=NULL, color=NULL), color="black", linetype=2)
+ggsave("../figures-scratch/coexpression-per-phylum-posteriors.pdf", width=7, height=4)
+
+
+v= merge(u, slopes %>% group_by(Genus) %>% summarise(Slope = mean(Coefficient)), by=c("Genus"))
+
+plot = ggplot(v, aes(x=OSSameSide_Real_Prob, y=Coexpression)) + geom_point() + geom_segment(aes(x=c(0), xend=c(1), y=c(0.24 - (-0.1008315) * 0.4457977), yend=c(0.24+ (-0.1008315) * (1-0.4457977))), linetype=2) + geom_segment(aes(x=c(0), xend=c(1), y=c(0.24 - Slope * 0.4457977), yend=c(0.24+ Slope * (1-0.4457977)))) +  facet_wrap(~Genus)
+ggsave("../figures-scratch/coexpression-per-phylum-fits.pdf", width=5, height=5)
 
 
 
