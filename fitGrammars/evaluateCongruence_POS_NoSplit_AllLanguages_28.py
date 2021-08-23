583390404c25193b36953eaa72484487973277e3
Michael Hahn <mhahn29@gmail.com>
2021-08-22 19:22:48 -0700

    code

---
+++ fitGrammars/evaluateCongruence_POS_NoSplit_AllLanguages_28.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+import sys
+keys = {}
+import sys
+
+import random
+import subprocess
+
+from ud_languages_28 import languages
+
+script = "evaluateCongruence_POS_NoSplit.py"
+
+with open(f"output/{script}.tsv", "r") as inFile:
+   next(inFile)
+   done_languages = [x.split("\t")[0] for x in inFile]
+#with open(f"output/{script}.tsv", "w") as outFile:
+#  print("\t".join(["Language", "Model", "Congruence_All", "Congruence_VN", "Congruence_VP", "Congruence_V", "Congruence_VProp"]), file=outFile)
+for language in languages:
+  if language in done_languages:
+    continue
+  subprocess.call(['/u/nlp/anaconda/ubuntu_16/envs/py27-mhahn/bin/python2.7', script, "--language="+language])
+
